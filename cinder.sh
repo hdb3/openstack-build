10ac4648376fdfa871689b553c697e55c4e676fc
Nicholas Hart <nic@lancs>
2015-10-23 15:21:45 +0100

    cinder.sh was not saved b4?

--- cinder.sh
+++ cinder.sh
@@ -1,37 +1,31 @@
 source config
 source creds
-#install cinder controller
 
 #cp /usr/share/cinder/cinder-dist.conf /etc/cinder/cinder.conf
 chown -R cinder:cinder /etc/cinder/cinder.conf
 
-#edit /etc/cinder/cinder.conf
-sed -i.bak "/\[database\]/a connection = mysql://cinder:$SERVICE_PASSWD@$CONTROLLER_IP/cinder" /etc/cinder/cinder.conf
+crudini --set --verbose /etc/cinder/cinder.conf database connection mysql://cinder:$DBPASSWD@$CONTROLLER_IP/cinder
 
-sed -i "/^\[DEFAULT\]/a \
-rpc_backend = rabbit\n\
-my_ip = $CONTROLLER_IP\n\
-auth_strategy = keystone" /etc/cinder/cinder.conf
+crudini --set --verbose /etc/cinder/cinder.conf DEFAULT rpc_backend rabbit
+crudini --set --verbose /etc/cinder/cinder.conf DEFAULT my_ip $CONTROLLER_IP
+crudini --set --verbose /etc/cinder/cinder.conf DEFAULT auth_strategy keystone
 
-sed -i "/^\[oslo_messaging_rabbit\]/a \
-rabbit_host = $CONTROLLER_IP\n\
-rabbit_userid = openstack\n\
-rabbit_password =$SERVICE_PASSWD" /etc/cinder/cinder.conf
+crudini --set --verbose /etc/cinder/cinder.conf oslo_messaging_rabbit rabbit_host $CONTROLLER_IP
+crudini --set --verbose /etc/cinder/cinder.conf oslo_messaging_rabbit rabbit_userid openstack
+crudini --set --verbose /etc/cinder/cinder.conf oslo_messaging_rabbit rabbit_password $SERVICE_PWD
 
-sed -i "/^\[oslo_concurrency\]/a \
-lock_path = /var/lock/cinder" /etc/cinder/cinder.conf
+crudini --set --verbose /etc/cinder/cinder.conf oslo_concurrency lock_path /var/lock/cinder
 
-sed -i "/^\[keystone_authtoken\]/a \
-auth_uri = http://$CONTROLLER_IP:5000\n\
-auth_url = http://$CONTROLLER_IP:35357\n\
-auth_plugin = password\n\
-project_domain_id = default\n\
-user_domain_id = default\n\
-project_name = service\n\
-username = cinder\n\
-password = $SERVICE_PASSWD" /etc/cinder/cinder.conf
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken auth_uri http://$CONTROLLER_IP:5000
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken auth_url http://$CONTROLLER_IP:35357
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken auth_plugin password
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken project_domain_id default
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken user_domain_id default 
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken project_name service
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken username cinder
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken password $SERVICE_PWD
+crudini --set --verbose /etc/cinder/cinder.conf keystone_authtoken
 
-#start cinder controller
 su -s /bin/sh -c "cinder-manage db sync" cinder
-systemctl enable openstack-cinder-api.service openstack-cinder-scheduler.service
-systemctl start openstack-cinder-api.service openstack-cinder-scheduler.service
+systemctl enable openstack-cinder-api openstack-cinder-scheduler
+systemctl start openstack-cinder-api openstack-cinder-scheduler
