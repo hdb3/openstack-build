6ed0c071fa5081b60844548bbe4ce5ee96f3afd7
Nicholas Hart <nic@lancs>
2015-10-23 15:21:06 +0100

    role.py ready for real work?

--- role.py
+++ role.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import netifaces
+from sys import exit, stderr
+import colorama
 
 def getaddrs():
     addrs = []
@@ -12,48 +14,78 @@ def getaddrs():
 
 def getroles():
     roles = {}
-    infile = open("roles","r")
+    try:
+        infile = open("roles","r")
+    except:
+        print >> stderr, cfr + "Hmmm, I had a teensy problem trying to find your role configuration file: is there a file somewhere here called 'role'?"
+        exit(1)
     for line in infile.readlines():
-        words = line.split()
-        if len(words) > 1:
-            roles[words[0]] = words[1]
+        if line[0] != '#':
+            words = line.split()
+            if len(words) > 1:
+                roles[words[0]] = words[1]
     return roles
 
 # first process the roles without considering local roles issues
 
-roleaddrs = getroles()
-roles = {}
-for addr,role in roleaddrs.iteritems():
-    if role not in roles:
-        roles[role] = []
-    roles[role].append(addr)
+def main():
+    envstrings = []
+    roleaddrs = getroles()
+    roles = {}
+    for addr,role in roleaddrs.iteritems():
+        if role not in roles:
+            roles[role] = []
+        roles[role].append(addr)
 
-print "the following roles were found:"
-for role, addrs in roles.iteritems():
-    print "role %s: " % role,
-    print addrs
+    print >> stderr, cfg + "the following roles were found:"
+    for role, addrs in roles.iteritems():
+        print >> stderr, cfg, "role %s: " % role,
+        print >> stderr, cfg , addrs
 
-if 'controller' not in roles:
-    print "Warning! - no controller was found - cannot set controller IP"
-else:
-    controller_ip = roles['controller'][0]
-    if len(roles['controller']) > 1:
-        print "Warning! - more than one controller was found - using first candidate for controller IP (%s)" % controller_ip
-
-
-# now grab a list of lcal interface addresses and see if we match any of them to our role list from the configuratioon file
-
-addrs = getaddrs()
-my_roles = []
-for (iface,addr) in addrs:
-    if addr in roleaddrs:
-        my_roles.append((roleaddrs[addr],addr,iface))
-        # print "role found: %s (address %s on interface %s)" % (roleaddrs[addr],addr,iface)
-if len(my_roles) == 0:
-    print "Warning - no role was found based on local addresses for this host"
-else:
-    if len(my_roles) == 1:
-        print "Happy days! - a role was found based on local addresses for this host - role found: %s (address %s on interface %s)" % my_roles[0]
+    if 'controller' not in roles:
+        print >> stderr, cfr + "Warning! - no controller was found - cannot set controller IP"
     else:
-        print "Warning - multiple roles found based on local addresses for this host",
-        print my_roles
+        controller_ip = roles['controller'][0]
+        envstrings.append(("CONTROLLER_IP",controller_ip))
+        if len(roles['controller']) > 1:
+            print >> stderr, cfy + "Warning! - more than one controller was found - using first candidate for controller IP (%s)" % controller_ip
+
+
+    # now grab a list of lcal interface addresses and see if we match any of them to our role list from the configuratioon file
+
+    addrs = getaddrs()
+    my_roles = []
+    for (iface,addr) in addrs:
+        if addr in roleaddrs:
+            my_roles.append((roleaddrs[addr],addr,iface))
+            # print >> stderr, "role found: %s (address %s on interface %s)" % (roleaddrs[addr],addr,iface)
+    if len(my_roles) == 0:
+        print >> stderr, cfr + "Warning - no role was found based on local addresses for this host"
+    else:
+        envstrings.append(("MY_IP",my_roles[0][1]))
+        envstrings.append(("MY_ROLE",my_roles[0][0]))
+        if len(my_roles) == 1:
+            print >> stderr, cfg + "Happy days! - a role was found based on local addresses for this host - role found: %s (address %s on interface %s)" % my_roles[0]
+        else:
+            print >> stderr, cfy + "Warning - multiple roles found based on local addresses for this host",
+            print >> stderr, my_roles
+    return envstrings
+# end of main!
+
+
+cfr = colorama.Fore.RED
+cfg = colorama.Fore.GREEN
+cfy = colorama.Fore.YELLOW
+colorama.init()
+
+try:
+    envstrings = main()
+except:
+    print >> stderr,colorama.Fore.RESET
+    colorama.deinit()
+else:
+    print >> stderr,colorama.Fore.RESET
+    # print colorama.Fore.RESET
+    colorama.deinit()
+    for envval in envstrings:
+        print "export %s=%s"  % (envval)
