31167779da6c0b731aaf2c2612d9a99673b8cb67
Nicholas Hart <nic@lancs>
2015-10-26 17:43:02 +0000

    running fixes

--- network.sh
+++ network.sh
@@ -1,7 +1,7 @@
 echo 'net.ipv4.conf.all.rp_filter=0' >> /etc/sysctl.conf
 echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-iptables=1' >> /etc/sysctl.conf
-echo 'net.bridge.bridge-nf-call-ip6tables=1' >> /etc/sysctl.conf
+# echo 'net.bridge.bridge-nf-call-iptables=1' >> /etc/sysctl.conf
+# echo 'net.bridge.bridge-nf-call-ip6tables=1' >> /etc/sysctl.conf
 sysctl -p
 # crudini --set --verbose 
 NETWORK-SERVICES="openvswitch libvirtd openstack-nova-compute neutron-openvswitch-agent"
