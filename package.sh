2ce67c990a4de5608b3c06138ecae949259e59fb
Nicholas Hart <nic@lancs>
2015-10-26 17:21:34 +0000

    running fixes

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 
 yum -y install yum-plugin-priorities
 yum -y install epel-release
-yum -y install http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm
+yum -y install http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm || echo "yum install: nothing to do"
 yum -y upgrade
 if [[ $MY_ROLE == "controller" ]] ; then
  PACKAGES="openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler python-novaclient wget openstack-glance python-glance python-glanceclient openstack-keystone httpd mod_wsgi python-openstackclient memcached python-memcached rabbitmq-server mariadb mariadb-server MySQL-python ntp openstack-neutron openstack-neutron-ml2 python-neutronclient openstack-dashboard openstack-cinder python-cinderclient python-oslo-db"
