fa598510af289797f7e6f4931d56f2c970c8d681
Nicholas Hart <nic@lancs>
2015-10-26 17:45:28 +0000

    running fixes

--- compute.sh
+++ compute.sh
@@ -5,7 +5,7 @@ echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.conf
 sysctl -p
 yum -y install openstack-nova-compute sysfsutils openstack-neutron openstack-neutron-ml2 openstack-neutron-openvswitch
 crudini --set --verbose /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
-COMPUTE-SERVICES="openvswitch libvirtd openstack-nova-compute neutron-openvswitch-agent"
+COMPUTE_SERVICES="openvswitch libvirtd openstack-nova-compute neutron-openvswitch-agent"
 
-systemctl enable COMPUTE-SERVICES
-systemctl start COMPUTE-SERVICES
+systemctl enable COMPUTE_SERVICES
+systemctl start COMPUTE_SERVICES
--- network.sh
+++ network.sh
@@ -4,7 +4,7 @@ echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.conf
 # echo 'net.bridge.bridge-nf-call-ip6tables=1' >> /etc/sysctl.conf
 sysctl -p
 # crudini --set --verbose 
-NETWORK-SERVICES="openvswitch libvirtd openstack-nova-compute neutron-openvswitch-agent"
+NETWORK_SERVICES="openvswitch libvirtd openstack-nova-compute neutron-openvswitch-agent"
 
-systemctl enable NETWORK-SERVICES
-systemctl start NETWORK-SERVICES
+systemctl enable NETWORK_SERVICES
+systemctl start NETWORK_SERVICES
