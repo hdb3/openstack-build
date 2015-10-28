dd6312d2867ff3e94f335128a7fab3ed5be51e2a
Nicholas Hart <nic@lancs>
2015-10-28 17:24:23 +0000

    adding the subnet support for setting the local IP address for tunnels

--- remote.sh
+++ remote.sh
@@ -1,2 +1,2 @@
 #ssh-keygen -f "/home/nic/.ssh/known_hosts" -R 10.30.65.115 && ssh-keygen -f "/home/nic/.ssh/known_hosts" -R $1 && ssh -t $1 sudo yum -y install epel-release && ssh -t $1 sudo yum -y install git python-netifaces python-pip && ssh -t $1 sudo pip install colorama && ssh -t $1 git clone https://github.com/hdb3/openstack-build.git && ssh $1
-ssh -t $1 sudo yum -y install epel-release && ssh -t $1 sudo yum -y install python-netifaces python-pip && ssh -t $1 sudo pip install colorama && scp *sh config roles do_it role.py $1: && ssh -t $1 ./do_it
+ssh -t $1 sudo yum -y install epel-release && ssh -t $1 sudo yum -y install python-netifaces python-pip && ssh -t $1 sudo pip install colorama && scp *sh config roles do_it subnet.py role.py $1: && ssh -t $1 ./do_it
