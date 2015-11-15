d68996cdb9426f06d21534c942ba8f1951478ce3
Nicholas Hart <nic@lancs>
2015-11-15 11:40:25 +0000

    working for horizon install

--- horizon.sh
+++ horizon.sh
@@ -1,14 +1,6 @@
-
-#edit /etc/openstack-dashboard/local_settings
-sed -i.bak -e "s/ALLOWED_HOSTS = \['horizon.example.com', 'localhost'\]/ALLOWED_HOSTS = '*'/" /etc/openstack-dashboard/local_settings
-sed -i -e 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$CONTROLLER_IP"'"/' /etc/openstack-dashboard/local_settings
-
-sed -i "s/CACHES = {\n    'default': {\n        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',\n        'LOCATION': '127.0.0.1:11211',\n    }\n\}/" /etc/openstack-dashboard/local_settings
-
-sed -i 's/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/' /etc/openstack-dashboard/local_settings
-
-
+patch -d / -p1 < patch.horizon
+sed -n -e "/^OPENSTACK_HOST.*127.0.0.1/s/127.0.0.1/$CONTROLLER_IP/" /etc/openstack-dashboard/local_settings
 setsebool -P httpd_can_network_connect on
 chown -R apache:apache /usr/share/openstack-dashboard/static
-systemctl enable httpd.service memcached.service
-systemctl start httpd.service memcached.service
+systemctl enable httpd memcached
+systemctl start httpd memcached
--- remote.sh
+++ remote.sh
@@ -4,6 +4,6 @@ do
     echo "$node"
     ssh-keygen -f "/home/nic/.ssh/known_hosts" -R `dig +short $node`
     ssh-keygen -f "/home/nic/.ssh/known_hosts" -R $node
-    scp *sh config roles do_it subnet.py role.py $node:
+    scp *sh config roles do_it subnet.py role.py patch.horizon $node:
     ssh -t $node ./do_it
 done
